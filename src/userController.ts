6ed36a570ebb715de2a7bf58f5febb295cac190e
Jaemin <ghdckdwn8456@gmail.com>
2025-01-29 16:13:39 +0900

    [feat] 응답 수정

--- src/userController.ts
+++ src/userController.ts
@@ -18,7 +18,7 @@ export default class UserController {
             res.status(404).json({ message: "User not found" });
             return;
         }
-        res.status(200).json({ id: user.id, name: user.name, email: user.email, create_at: user.createAt });
+        res.status(200).json({ id: user.id, name: user.name, email: user.email, create_at: user.createAt, team: user.team, posts: user.posts });
     }
 
     async createUser(req: Request, res: Response) {
